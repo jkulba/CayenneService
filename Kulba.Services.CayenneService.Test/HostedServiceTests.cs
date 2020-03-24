a20a4f13e340af3d327b01129a3b68bd2a59a1c2
James Kulba <jkulba@alphatecspine.com>
2020-03-24 13:06:55 -0700

    Added xunit config file

--- Kulba.Services.CayenneService.Test/HostedServiceTests.cs
+++ Kulba.Services.CayenneService.Test/HostedServiceTests.cs
@@ -4,23 +4,28 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace Kulba.Services.CayenneService.Test
 {
     public class HostedServiceTests : IClassFixture<HostedServiceFixture>
     {
         private readonly ITestOutputHelper _output;
+        private readonly IMessageSink _diagnosticMessageSink;
         HostedServiceFixture _hostedServiceFixture;
 
-        public HostedServiceTests(ITestOutputHelper _testOutputHelper, HostedServiceFixture hostedServiceFixture)
+        public HostedServiceTests(ITestOutputHelper _testOutputHelper, HostedServiceFixture hostedServiceFixture, IMessageSink diagnosticMessageSink)
         {
             _output = _testOutputHelper;
             _hostedServiceFixture = hostedServiceFixture;
+            _diagnosticMessageSink = diagnosticMessageSink;
         }
 
         [Fact(DisplayName = "Successfully Load Applicaiton Configuration")]
         public void SuccessLoadConfiguration()
         {
+            var message = new DiagnosticMessage("Hello Joe Message Sink");
+            _diagnosticMessageSink.OnMessage(message);
             //Arrange
             _output.WriteLine("Arrange");
             
